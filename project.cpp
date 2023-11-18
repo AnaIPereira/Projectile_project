e905ae17e6c7b96dcfb4e667e49980a2e12df2f0
AnaIPereira <ana.pereira98@icloud.com>
2023-11-18 20:41:02 +0000

    functions

--- project.cpp
+++ project.cpp
@@ -6,25 +6,47 @@
 using namespace std;
 
 // function to return optimal angle
-double theta_c(double v0, double g, double d)
-{
-    double sr=1+2*g/(pow(v0,2))-pow(g,2)*pow(d,2)/pow(v0,4);
-    double x=pow(v0,2)/(2*g)*(1+sqrt(sr));
-    double thetac = atan(x);
-    return thetac;
+//double theta_c(double v0, double g, double d)
+//{
+  //  double sr=1 + 2*g/pow(v0,2)*(-0.6-1/2*g*pow(d,2)/pow(v0,2));
+    //double x=pow(v0,2)*(1 + sqrt(sr));
+    //double thetac = atan(x);
+    //return thetac;
+//}
+
+//opt angle
+
+void opt(){
+    double g=9.8;
+    double v=450;
+    double d=100;
+    double h=-0.6;
+    double a = -1/2*g*pow(d,2)/pow(v,2);
+    double b = d;
+    double c = h-1/2*g*pow(d,2)/pow(v,2);
+    double tang=-b+sqrt(pow(b,2)-4*a*c)/(2*a);
+    return atan(tang);
 }
 
 // function to return angle from gaussian function
-double rand_angle(double mean, double stddev) {
+double rand_angle(double mean, double stddev) 
+{
     random_device rd; 
     mt19937 gen(rd());
     normal_distribution<double> distribution(mean, stddev);
     return distribution(gen);
 } 
 
+// function to compute the high at wich the bullet hits the target
+double high(double h1, double v0, double g, double d, double angle)
+{
+    double y = h1 - 1/2*g*pow(d,2)/(pow(v0,2))*(1 + pow(tan(angle),2)) + tan(angle)*d ;
+    return y;
+}
 
 int main() 
 {
+// test theta_c function
     double vel=450;
     double grav=9.8;
     double x=100;
@@ -32,10 +54,20 @@ int main()
     double thet=theta_c(vel,grav,x);
     cout << "Optimal Angle: " << thet << " radians" << endl;
 
+// test rand_angle function
     double mean = 0;
     double stddev = 3*M_PI/180;
     double t = rand_angle(mean, stddev);
     cout << "Random Angle: " << t << " radians" << endl;
-    return 0;
+
+// teste high function
+    double h = 0.7;
+    double ang = thet + t;
+    double yy = high(h, vel, grav, x, ang);
+    cout << "theta  " << ang << " " << "high:  " << yy << "meters" << endl;
+
+
+    double ttt=opt;
+return 0;
 }
 
