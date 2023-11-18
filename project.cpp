247c012b38b6e29ae74fcfb35ade01ebdc2208a5
AnaIPereira <ana.pereira98@icloud.com>
2023-11-18 18:39:06 +0000

    func

--- project.cpp
+++ project.cpp
@@ -6,11 +6,12 @@
 using namespace std;
 
 // function to return optimal angle
-void theta_c( double v0, double g, double d)
+double theta_c(double v0, double g, double d)
 {
     double sr=1+2*g/(pow(v0,2))-pow(g,2)*pow(d,2)/pow(v0,4);
     double x=pow(v0,2)/(2*g)*(1+sqrt(sr));
     double thetac = atan(x);
+    return thetac;
 }
 
 // function to return angle from gaussian function
@@ -21,13 +22,20 @@ double rand_angle(double mean, double stddev) {
     return distribution(gen);
 } 
 
-int main() {
-    double mean = 100;
-    double stddev = 300;
 
+int main() 
+{
+    double vel=450;
+    double grav=9.8;
+    double x=100;
+
+    double thet=theta_c(vel,grav,x);
+    cout << "Optimal Angle: " << thet << " radians" << endl;
+
+    double mean = 0;
+    double stddev = 3*M_PI/180;
     double t = rand_angle(mean, stddev);
     cout << "Random Angle: " << t << " radians" << endl;
-
     return 0;
 }
 
