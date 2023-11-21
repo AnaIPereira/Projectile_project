d466e5ef77bdb062b36a8a67114a3aef82ceeff4
AnaIPereira <ana.pereira98@icloud.com>
2023-11-21 14:36:12 +0000

    function

--- project.cpp
+++ project.cpp
@@ -5,6 +5,23 @@
 #include <random>
 using namespace std;
 
+void write_out_vec(const string& filename, string& array_name , const vector<double>& output, bool initialise = true)
+// Writes out data in numpy form. If initialise == true, creates file from scratch and includes top lines needed
+{
+ofstream myfile;
+if (initialise){
+myfile.open (filename);
+myfile << "import numpy as np" << "\n" << "\n";}
+else{myfile.open (filename, std::ios_base::app);}
+myfile << array_name << " = np.array((" << "\n";
+for (auto i = 0; i < output.size(); ++i)
+{if (i != (output.size() - 1))
+{myfile << output.at(i) << ", " << "\n";}
+else
+{myfile << output.at(i) << "\n";}}
+myfile << "))" << "\n";
+myfile.close();}
+
 // function to return optimal angle
 double theta_c(double v0, double g, double d)
 {
@@ -31,42 +48,44 @@ double high(double h1, double v0, double g, double d, double angle)
 }
 
 // function that simulates 15 attemps as many times as we give in the argument
-double numb_sim(double n)
-{
-    // create vector that is going to save n atempts
+double numb_sim(double n) {
+    // create vector that is going to save n attempts
 
-    int tot = 15; //total number of attempts
-    int hit = 0; //counter for number of times that hits the target
-    vector<double> vec_hits(15,0); //vector that saves # of hits
-    vector<double> position; //vector that saves position
+    int tot = 15; // total number of attempts
+    vector<double> vec_hits; // vector that saves # of hits
     double mean = 0;
-    double stddev = 3*M_PI/180;
-    double thet=theta_c(450,9.8,100); // computes the optimal angle
-    for (int k = 0; k < n; k++)
-    {
-            for (int i = 0; i < tot; i++)
-    {
-     double t = rand_angle(mean, stddev); // computes the random angle
-     double angle = thet + t; // optimal angles plus random angle
-     double yy = high(0.7, 450, 9.8, 100, angle); // computes the high
-     position.push_back(yy); // vector that saves high for each attempt
-     //cout  << position[i] << "\n" << endl;
-     //cout << "theta  " << angle << " " << "high:  " << yy << "meters" << endl;
-    if(position[i] < 2.30 && position[i] > 0.30)
-    {
-            hit++; // in the end is the total number of hits
-    }
-    vec_hits.push_back(hit); // saves the total number of hits for 15 attempt
-    }   
-    cout << hit << endl;
-    return hit;
+    double stddev = 3 * M_PI / 180;
+    double thet = theta_c(450, 9.8, 100); // computes the optimal angle
+
+    for (int k = 0; k < n; k++) {
+        //cout << "trial  " << k << endl;
+        int hit = 0; // counter for the number of times that hit the target
+        vector<double> position; // vector that saves position
+
+        for (int i = 0; i < tot; i++) {
+            double t = rand_angle(mean, stddev); // computes the random angle
+            double angle = thet + t; // optimal angles plus random angle
+            double yy = high(0.7, 450, 9.8, 100, angle); // computes the high
+            position.push_back(yy); // vector that saves high for each attempt
+
+            if (position[i] < 2.30 && position[i] > 0.30) {
+                hit++; // in the end is the total number of hits
+            }
+        }
+        vec_hits.push_back(hit); // saves the total number of hits for 15 attempt
     }
 
+    cout << "vector size  " << vec_hits.size() << endl;
+    for (int p = 0; p < vec_hits.size(); p++)
+    {
+        cout << "number of hits  " << vec_hits[p] << endl;
+    }
 }
+
 
 // MAIN FUNCTION
 
-//
+
 int main() 
 {
 // test theta_c function
@@ -89,8 +108,7 @@ int main()
     //cout << "theta  " << ang << " " << "high:  " << yy << "meters" << endl;
 
 // test numb_sim function
-
-    double test = numb_sim(2);
+    double test = numb_sim(1000);
 
 return 0;
 }
